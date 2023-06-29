74240b32723aa3c4a57425f2732dedb1060f9c5d
Crowdbotics <engineering@crowdbotics.com>
2023-06-29 12:58:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solitary-wood-dev-95965",
+  url: "https://solitary-wood-dev-95965.botics.co",
+  api: "https://solitary-wood-dev-95965.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
